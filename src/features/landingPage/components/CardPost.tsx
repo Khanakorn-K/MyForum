Khanakorn-K/setupProjectNextJs

improve cardpost ui

src/features/landingPage/components/CardPost.tsx
@@ -1,53 +1,61 @@
 import mockImageData from "@/utils/mockImageData";
 import Image from "next/image";
 import { PostListentity } from "../entity/PostListentity";
-import { Button } from "@/components/ui/Button";
+import { Calendar, ArrowRight, Clock } from "lucide-react";
+
+// If Badge doesn't exist, I will use a span with tailwind classes.
+// Checking previous file content, it used a span. I'll stick to standard HTML/Tailwind for safety unless I see a Badge import in other files.
+// Actually, I'll use standard Tailwind for the tag to be safe and self-contained.
 
 const CardPost = ({
   content = "นี่คือ mock content สำหรับโพสต์ตัวอย่าง",
   coverImage = "",
-  displayDisplayCreateAt = "2024-01-01T00:00:00.000Z",
+  displayDisplayCreateAt = "2024-01-01",
   slug = ["mock-post"],
   title = "Mock Post Title",
 }: Partial<PostListentity>) => {
   return (
-    <article className="group flex flex-col overflow-hidden rounded-2xl border border-slate-200 bg-white shadow-sm transition hover:-translate-y-1 hover:shadow-lg">
-      {/* รูปด้านบน */}
-      <div className="relative h-52 w-full overflow-hidden bg-slate-100">
+    <article className="group relative flex flex-col overflow-hidden rounded-2xl bg-card text-card-foreground shadow-sm ring-1 ring-border transition-all duration-300 hover:-translate-y-1 hover:shadow-md">
+      {/* Image Section */}
+      <div className="relative aspect-video w-full overflow-hidden bg-muted">
         <Image
-          width={1200}
-          height={800}
-          src={coverImage ?? mockImageData()}
-          alt={title ?? ""}
-          className="h-full w-full object-contain transition duration-300 group-hover:scale-105"
+          width={800}
+          height={450}
+          src={coverImage || mockImageData()}
+          alt={title || "Post cover"}
+          className="h-full w-full object-cover transition-transform duration-500 group-hover:scale-105"
         />
-        {/* แท็กเล็ก ๆ มุมบนซ้าย */}
-        <div className="absolute left-3 top-3">
-          <span className="rounded-full bg-black/70 px-3 py-1 text-xs font-medium text-white mx-1">
-            {slug}
+        <div className="absolute inset-0 bg-gradient-to-t from-black/60 to-transparent opacity-0 transition-opacity duration-300 group-hover:opacity-100" />
+
+        {/* Tag */}
+        <div className="absolute left-4 top-4">
+          <span className="inline-flex items-center rounded-full bg-background/90 px-2.5 py-0.5 text-xs font-medium text-foreground backdrop-blur-sm shadow-sm">
+            {Array.isArray(slug) ? slug[0] : slug}
           </span>
         </div>
       </div>
 
-      {/* เนื้อหาด้านล่าง */}
-      <div className="flex flex-1 flex-col gap-2 px-4 py-4">
-        <h3 className="line-clamp-2 text-lg font-semibold text-slate-900">
-          {/* //ถ้าเกิน 60 ตัวให้แสดง ... พ้วงท้าย */}
+      {/* Content Section */}
+      <div className="flex flex-1 flex-col p-5">
+        <div className="flex items-center gap-2 text-xs text-muted-foreground mb-3">
+          <Calendar className="h-3.5 w-3.5" />
+          <time dateTime={displayDisplayCreateAt}>{displayDisplayCreateAt}</time>
+        </div>
+
+        <h3 className="mb-2 line-clamp-2 text-xl font-bold tracking-tight text-foreground transition-colors group-hover:text-primary">
           {title}
         </h3>
 
-        <p className="line-clamp-3 text-sm text-slate-500">
-          {content!.length > 60
-            ? content?.substring(0, 60) + "..."
-            : content ?? ""}
+        <p className="mb-4 line-clamp-2 flex-1 text-sm text-muted-foreground leading-relaxed">
+          {content}
         </p>
 
-        {/* footer เล็ก ๆ ด้านล่าง */}
-        <div className="mt-3 flex items-center justify-between text-xs text-slate-400">
-          <Button variant="secondary">
-            <h1>อ่านต่อ...</h1>
-          </Button>
-          <p>{displayDisplayCreateAt}</p>
+        {/* Footer */}
+        <div className="mt-auto flex items-center justify-between border-t border-border pt-4">
+          <div className="flex items-center text-sm font-medium text-primary">
+            Read article
+            <ArrowRight className="ml-1 h-4 w-4 transition-transform group-hover:translate-x-1" />
+          </div>
         </div>
       </div>
     </article>

Khanakorn-K/MyForum

src/features/landingPage/components/CardPost.tsx
@@ -1,53 +1,80 @@
 import mockImageData from "@/utils/mockImageData";
 import Image from "next/image";
 import { PostListentity } from "../entity/PostListentity";
+import { Calendar, ArrowRight, Clock } from "lucide-react";
+import Link from "next/link";
+import { useSession } from "next-auth/react";
 import { Button } from "@/components/ui/Button";
-
 const CardPost = ({
+  id = "1",
   content = "นี่คือ mock content สำหรับโพสต์ตัวอย่าง",
   coverImage = "",
-  displayDisplayCreateAt = "2024-01-01T00:00:00.000Z",
+  displayDisplayCreateAt = "2024-01-01",
   slug = ["mock-post"],
   title = "Mock Post Title",
+  authorImage = mockImageData(),
+  authorName = "anonymounse",
 }: Partial<PostListentity>) => {
+  const session = useSession();
   return (
-    <article className="group flex flex-col overflow-hidden rounded-2xl border border-slate-200 bg-white shadow-sm transition hover:-translate-y-1 hover:shadow-lg">
-      {/* รูปด้านบน */}
-      <div className="relative h-52 w-full overflow-hidden bg-slate-100">
+    <article className="group w-1/2 h-1/2 relative flex flex-col overflow-hidden rounded-2xl bg-card text-card-foreground shadow-sm ring-1 ring-border transition-all duration-300 hover:-translate-y-1 hover:shadow-md">
+      {/* Image Section */}
+      <div className="relative aspect-video w-full overflow-hidden bg-muted">
         <Image
-          width={1200}
-          height={800}
-          src={coverImage ?? mockImageData()}
-          alt={title ?? ""}
-          className="h-full w-full object-contain transition duration-300 group-hover:scale-105"
+          width={800}
+          height={450}
+          src={coverImage || mockImageData()}
+          alt={title || "Post cover"}
+          className="h-full w-full object-contain transition-transform duration-500 group-hover:scale-105"
         />
-        {/* แท็กเล็ก ๆ มุมบนซ้าย */}
-        <div className="absolute left-3 top-3">
-          <span className="rounded-full bg-black/70 px-3 py-1 text-xs font-medium text-white mx-1">
-            {slug}
+        <div className="absolute inset-0 bg-gradient-to-t from-black/60 to-transparent opacity-0 transition-opacity duration-300 group-hover:opacity-100" />
+
+        {/* Tag */}
+        <div className="absolute left-4 top-4">
+          <span className="inline-flex items-center rounded-full bg-background/90 px-2.5 py-0.5 text-xs font-medium text-foreground backdrop-blur-sm shadow-sm">
+            {Array.isArray(slug) ? slug[0] : slug}
           </span>
         </div>
       </div>
 
-      {/* เนื้อหาด้านล่าง */}
-      <div className="flex flex-1 flex-col gap-2 px-4 py-4">
-        <h3 className="line-clamp-2 text-lg font-semibold text-slate-900">
-          {/* //ถ้าเกิน 60 ตัวให้แสดง ... พ้วงท้าย */}
+      {/* Content Section */}
+      <div className="flex flex-1 flex-col p-5">
+        <div className="flex items-center gap-2 text-xs text-muted-foreground mb-3">
+          <Calendar className="h-3.5 w-3.5" />
+          <time dateTime={displayDisplayCreateAt}>
+            {displayDisplayCreateAt}
+          </time>
+        </div>
+
+        <h3 className="mb-2 line-clamp-2 text-xl font-bold tracking-tight text-foreground transition-colors group-hover:text-primary">
           {title}
         </h3>
 
-        <p className="line-clamp-3 text-sm text-slate-500">
-          {content!.length > 60
-            ? content?.substring(0, 60) + "..."
-            : content ?? ""}
+        <p className="mb-4 line-clamp-2 flex-1 text-sm text-muted-foreground leading-relaxed">
+          {content}
         </p>
 
-        {/* footer เล็ก ๆ ด้านล่าง */}
-        <div className="mt-3 flex items-center justify-between text-xs text-slate-400">
-          <Button variant="secondary">
-            <h1>อ่านต่อ...</h1>
-          </Button>
-          <p>{displayDisplayCreateAt}</p>
+        {/* Footer */}
+        <div className="mt-auto flex items-center justify-between border-t border-border pt-4">
+          <Link href={`/post?id=${id}`}>
+            <div className="flex items-center text-sm font-medium text-primary">
+              Read article
+              <ArrowRight className="ml-1 h-4 w-4 transition-transform group-hover:translate-x-1" />
+            </div>
+          </Link>
+          <div className=" flex flex-row gap-2 items-center">
+            <Image
+              src={authorImage}
+              width={50}
+              height={50}
+              alt={authorName}
+              className="rounded-full w-10 h-10"
+            />
+            <h2>ผู้เขียน : {authorName}</h2>
+            <Link className="bg-red-600 p-2 rounded-full" href={`createPost?postId=${id}`}>
+              <p>แก้ไข้</p>
+            </Link>
+          </div>
         </div>
       </div>
     </article>